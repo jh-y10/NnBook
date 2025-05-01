c6418f23ae99f9480ab6f151ec9b3f1a7a97a570
leealswl <alswl54321600@gmail.com>
2025-05-01 23:38:21 +0900

    error modify

--- frontend/src/hooks/useBooks.js
+++ frontend/src/hooks/useBooks.js
@@ -15,7 +15,6 @@ const fetchBookList = (query, page = 1, size = 20) => {
 };
 
 export default function useBooks(query, page = 1, size = 20) {
-<<<<<<< HEAD
     return useQuery({
         queryKey:["bookList", query, page, size],
         queryFn:() => fetchBookList(query, page, size),
@@ -27,16 +26,3 @@ export default function useBooks(query, page = 1, size = 20) {
         //staleTime:        1000 * 60 * 5,
       });
     }
-=======
-  return useQuery({
-    queryKey: ["bookList", query, page, size],
-    queryFn: () => fetchBookList(query, page, size),
-    select: (result) => {
-      //console.log("result.data", result.data.item);
-      return result.data.item;
-    },
-    keepPreviousData: true,
-    //staleTime:        1000 * 60 * 5,
-  });
-}
->>>>>>> 8baa75f3375ee1417652b8be165cd054596ad29d
