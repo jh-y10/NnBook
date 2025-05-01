268e4c1f2faf5e4bbb1887f18bf06b3912ac7601
JunHyeok Yoon <jhy230912@gmail.com>
2025-05-01 22:06:50 +0900

    not found update

--- src/pages/Home/Recommend.jsx
+++ src/pages/Home/Recommend.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../../styles/Recommend.style.css";
 
 const Recommend = () => {
   return (
-    <div>Recommend</div>
-  )
-}
+    <Container>
+      <Row>
+        <Col lg={12}>
+          <h1 className="recommend-title">이런 도서는 어떠신가요?</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Recommend
+export default Recommend;
