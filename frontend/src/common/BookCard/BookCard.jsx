53db007d6597af1563e956a202e1a16651b5612e
JunHyeok Yoon <jhy230912@gmail.com>
2025-05-02 17:50:56 +0900

    ui fix

--- frontend/src/common/BookCard/BookCard.jsx
+++ frontend/src/common/BookCard/BookCard.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Col } from 'react-bootstrap'
-import '/src/styles/BookCard.style.css';
-import PropTypes from 'prop-types'
+import React from "react";
+import { Col } from "react-bootstrap";
+import "/src/styles/BookCard.style.css";
+import PropTypes from "prop-types";
 
 export default function BookCard({ book, onClick }) {
   return (
@@ -10,7 +10,7 @@ export default function BookCard({ book, onClick }) {
         src={book.cover}
         alt={book.title}
         className="img-fluid book-cover-img"
-        style={{ cursor: onClick ? 'pointer' : 'default' }}
+        style={{ cursor: onClick ? "pointer" : "default" }}
         onClick={onClick}
       />
       <h6 className="mt-2 truncate" title={book.title}>
@@ -18,8 +18,5 @@ export default function BookCard({ book, onClick }) {
       </h6>
       <p className="text-muted truncate">{book.author}</p>
     </Col>
-  )
+  );
 }
-
-
-export default BookCard
