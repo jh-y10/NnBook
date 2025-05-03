04e1e5437b8ab4cec07e68d30077d46b6583a9a6
JiHyoung You <fbwlgud51@gmail.com>
2025-05-04 03:50:42 +0900

    Feat: add Book lend feature with emails

---
+++ frontend/src/hooks/useRegisterBookLendMutation.js
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import authApi from "../utils/authApi";
+
+const registerBookLend = async ({ bookID, ownerEmail, holderEmail }) => {
+  const res = await authApi.patch(`/library/lend`, {
+    bookID,
+    ownerEmail,
+    holderEmail,
+  });
+  return res.data;
+};
+
+export default function useRegisterBookLendMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: registerBookLend,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["books-register"]);
+      alert("대여 등록이 완료되었습니다.");
+    },
+    onError: (err) => {
+      alert(err.response?.data?.message || "대여 등록 중 오류 발생");
+    },
+  });
+}
