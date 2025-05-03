04f22a697208f4880e2cbf6c68138da7cfde9f14
JiHyoung You <fbwlgud51@gmail.com>
2025-05-03 22:34:04 +0900

    완독 도서 불러오기

--- frontend/src/pages/Library/MyLibrary.jsx
+++ frontend/src/pages/Library/MyLibrary.jsx
@@ -3,6 +3,7 @@ import "../../styles/MyLibrary.style.css";
 import { useMyInfoQuery } from "../../hooks/useMyInfoQuery";
 import { Alert } from "react-bootstrap";
 import { useReadingBookQuery } from "../../hooks/userReadingBookQuery";
+import { useFinishedBooksQuery } from "../../hooks/useFinishedBooksQuery";
 
 const MyLibrary = () => {
   const { data: mydata, isLoading, isError, error } = useMyInfoQuery();
@@ -12,6 +13,15 @@ const MyLibrary = () => {
     isLoading: isReadingLoading,
     isError: isReadingError,
   } = useReadingBookQuery(mydata?.email);
+
+  const ownerEmail = mydata.email;
+  const holderEmail = mydata.email;
+
+  const {
+    data: finishedBooks,
+    isLoading: isFinishedLoading,
+    isError: isFinishedError,
+  } = useFinishedBooksQuery({ ownerEmail, holderEmail });
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -45,7 +55,25 @@ const MyLibrary = () => {
       </div>
       <div className="section mt-5">
         <h3 className="libraryTitle mb-3">완독 도서</h3>
-        <div className="libraryBoxStroke libraryBookList">완독 도서 리스트</div>
+        <div className="libraryBoxStroke libraryBookList">
+          {isFinishedLoading ? (
+            <p>로딩 중...</p>
+          ) : isFinishedError ? (
+            <p>데이터를 불러오는 데 실패했습니다.</p>
+          ) : finishedBooks && finishedBooks.length > 0 ? (
+            finishedBooks.map((book) => (
+              <div key={book.id} className="bookItem">
+                <img
+                  className="libraryDetailBookImage"
+                  src={book.cover}
+                  alt={book.title ? book.title.split(" - ")[0] : ""}
+                />
+              </div>
+            ))
+          ) : (
+            <p>완독한 도서가 없습니다.</p>
+          )}
+        </div>
       </div>
       <div className="section mt-5">
         <h3 className="libraryTitle mb-3">빌려준 도서</h3>
