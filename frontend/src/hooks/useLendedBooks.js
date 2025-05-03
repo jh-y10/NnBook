e157be04359059744c5195e0b1cefcd22e395ef5
JiHyoung You <fbwlgud51@gmail.com>
2025-05-04 00:52:36 +0900

    fix: lended books

--- frontend/src/hooks/useLendedBooks.js
+++ frontend/src/hooks/useLendedBooks.js
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import authApi from "../utils/authApi";
 
-export const useLendedBooksQuery = (ownerEmail) => {
+const fetchLendedBooks = async () => {
+  const res = await authApi.get("/library/lended");
+  return res.data;
+};
+
+export const useLendedBooksQuery = () => {
   return useQuery({
-    queryKey: ["myLibrary", "lended", ownerEmail],
-    queryFn: async () => {
-      const { data } = await authApi.get(
-        `/library/lended?ownerEmail=${ownerEmail}`
-      );
-      return data; // 빌려준 도서 리스트
-    },
-    enabled: !!ownerEmail,
+    queryKey: ["books-lended"],
+    queryFn: fetchLendedBooks,
+    retry: false, // 토큰 오류 시 무한 재시도 방지
   });
 };
--- frontend/src/pages/Library/MyLibrary.jsx
+++ frontend/src/pages/Library/MyLibrary.jsx
@@ -5,17 +5,19 @@ import { Alert } from "react-bootstrap";
 import SingleLineCarousel from "../../common/react-multi-carousel/SingleLineCarousel";
 import { useReadingBooksQuery } from "../../hooks/useReadingBooks";
 import { useFinishedBooksQuery } from "../../hooks/useFinishedBooks";
+import { useLendedBooksQuery } from "../../hooks/useLendedBooks";
 
 const MyLibrary = () => {
   const { data: mydata, isLoading, isError, error } = useMyInfoQuery();
 
   const { data: readingdata } = useReadingBooksQuery();
-  const { data: finisheddata } = useFinishedBooksQuery;
+  const { data: finisheddata } = useFinishedBooksQuery();
+  const { data: lendeddata } = useLendedBooksQuery();
 
   console.log("data", mydata);
   console.log("rb", readingdata);
   console.log("fb", finisheddata);
-  // console.log("lb", lendedBooks);
+  console.log("lb", lendeddata);
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -47,7 +49,13 @@ const MyLibrary = () => {
       </div>
       <div className="section mt-5">
         <h3 className="libraryTitle mb-3">빌려준 도서</h3>
-        <div className="libraryBoxStroke libraryBookList"></div>
+        <div className="libraryBoxStroke libraryBookList">
+          <div>
+            {lendeddata?.length > 0 && (
+              <SingleLineCarousel books={lendeddata} />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
