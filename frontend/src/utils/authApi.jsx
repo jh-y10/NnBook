9371704a705fda5fe1b21cfc125eaeea49f3b0bb
cherrie <cherry199848@gmail.com>
2025-05-04 16:04:14 +0900

    modified server url

--- frontend/src/utils/authApi.jsx
+++ frontend/src/utils/authApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5050/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://nnbook-production.up.railway.app:8080/api",
   timeout: 5000,
 });
 
