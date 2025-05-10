146efa1db421b7bf8e65d33eedc5356c0eaffe11
cherrie <cherry199848@gmail.com>
2025-05-11 00:15:37 +0900

    fix: origin url 주소 수정

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: "https://nn-book.vercel.app"}));
+app.use(cors({ origin: "https://nn-book-seven.vercel.app" }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
