619882475035e683fe4b4f6e6be78ac399dc7826
cherrie <cherry199848@gmail.com>
2025-05-11 00:03:57 +0900

    fix: pool에서 port 추가

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
